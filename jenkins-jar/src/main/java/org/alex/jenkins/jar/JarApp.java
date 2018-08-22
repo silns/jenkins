ee0aaaf726d5b82347d4b49be3a18328a48e2e28
silns <310804919@qq.com>
2018-08-22 11:20:15 +0800

    m2

--- jenkins-jar/src/main/java/org/alex/jenkins/jar/JarApp.java
+++ jenkins-jar/src/main/java/org/alex/jenkins/jar/JarApp.java
@@ -4,5 +4,6 @@ public class JarApp {
     public static void main(String[] args) {
         System.out.println("Jenkins Jar App");
         System.out.println("Update one, test for hook trigger");
+        System.out.println("Update two, test for hook trigger");
     }
 }
